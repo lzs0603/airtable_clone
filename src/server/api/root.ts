1be07074855c01d0b9cdcd2684b10974e4b86282
jiangzegege123 <161683111+jiangzegege123@users.noreply.github.com>
2025-05-14 16:18:09 +1000

    create

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,16 +1,12 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { baseRouter } from "./routers/base";
-import { authRouter } from "./routers/auth";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({
-  base: baseRouter,
-  auth: authRouter,
-});
+export const appRouter = createTRPCRouter({ base: baseRouter });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
